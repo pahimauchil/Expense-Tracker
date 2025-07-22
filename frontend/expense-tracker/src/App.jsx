bb086971e9f20a6ef82fac34d9a8c75b6b345ebc
Pahima R Uchil <uchilpahi@gmail.com>
2025-07-22 19:48:11 +0530

    Inital project setup

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default App;
